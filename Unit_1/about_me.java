0b1a600f188bfcdb9eb693a0ce2f219b3f22d93e
Adam Stiles <115672504+AdamHStiles@users.noreply.github.com>
2023-08-16 09:42:58 -0400

    Add files via upload

---
+++ Unit_1/about_me.java
@@ -0,0 +1,38 @@
+package Unit_1;
+class respones{
+    public static void main (String[] args){
+        System.out.println("");
+        System.out.println("--------------------------------------");
+        System.out.println("");
+        
+        System.out.println("My answers:");
+        System.out.println("Name: Adam Stiles");
+        System.out.println("Hobbies: Swim team, Programming, Guitar, Cooking");
+        System.out.println("Color: Navy blue");
+        System.out.println("Book: Bible");
+        // My ansewrs
+        System.out.println("");
+        System.out.println("--------------------------------------");
+        System.out.println("");
+
+        System.out.println("Tyler's answers:");
+        System.out.println("Name: Tyler");
+        System.out.println("Hobbies: Vidoe Games");
+        System.out.println("Color: Blue/Purple");
+        System.out.println("Favorite game: Rust");
+        // Tyler's ansewrs
+        System.out.println("");
+        System.out.println("--------------------------------------");
+        System.out.println("");
+
+        System.out.println("Dagger's answers:");
+        System.out.println("Name: Dagger");
+        System.out.println("Hobbies: Staying at friend's houses");
+        System.out.println("Color: Purple");
+        System.out.println("Book: Daniel's Dilemma");
+        // Dagger's answers
+        System.out.println("");
+        System.out.println("--------------------------------------");
+        System.out.println("");
+    }
+}
---
+++ Unit_1/hello_world.java
@@ -0,0 +1,6 @@
+package Unit_1;
+public class hello_world {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}   
